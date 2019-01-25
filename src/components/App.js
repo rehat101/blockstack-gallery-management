b1a9685e96521da40fbab9f3fd6640c6d6f05a18
Taher <taher.dasten@gmail.com>
2019-01-25 15:35:02 +0530

    added support for protected routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component, Suspense } from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Spinner from './Spinner';
+import { isUserSignedIn } from 'blockstack';
 
 const Dashboard = React.lazy(() => import('./Dashboard'));
 const ArtworkPage = React.lazy(() => import('./ArtworkPage'));
+
+const ProtectedRoute = ({isAllowed, ...rest}) => {
+  return(
+    isAllowed ? <Route {...rest} /> : <p>You are not authorized to view this page. Please login!</p>
+  );
+};
 
 class App extends Component {
 
   constructor(props) {
   	super(props);
-
   }
 
   render() {
@@ -17,8 +23,8 @@ class App extends Component {
       <Suspense fallback={<Spinner/>}>
          <Switch>
             <Route exact path='/app'><Redirect to="/app/dashboard"/></Route>
-            <Route exact path='/app/dashboard' render={() => <Dashboard/>}/>
-            <Route path='/app/artwork/:id' render={(params) => <ArtworkPage {...params} />}/>
+            <ProtectedRoute isAllowed={isUserSignedIn()} exact path='/app/dashboard' render={() => <Dashboard/>}/>
+            <ProtectedRoute isAllowed={isUserSignedIn()} path='/app/artwork/:id' render={(params) => <ArtworkPage {...params} />}/>
          </Switch>
       </Suspense>
     );
