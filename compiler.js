9f2d14b49f77a304e62cdc848220a0bb11cdcd30
Taher <taher.dasten@gmail.com>
2019-02-19 22:32:27 +0530

    added manifest loader test file

--- compiler.js
+++ compiler.js
@@ -1,15 +1,15 @@
 const webpack = require('webpack');
 const memoryfs = require('memory-fs');
 
-const webpackConfig = require('../config/webpack.prod.config.js');
+const webpackConfig = require('./config/webpack.prod.config.js');
 const compiler = webpack(webpackConfig);
-console.log(webpackConfig);
+
 compiler.outputFileSystem = new memoryfs();
 
 return new Promise((resolve, reject) => {
   compiler.run((err, stats) => {
     if (err || stats.hasErrors()) reject(err);
-console.log(stats);
+
     resolve(stats);
   });
 });
--- manifest-loader/index.js
+++ manifest-loader/index.js
@@ -9,10 +9,12 @@ module.exports = function(source) {
   }
 
   const merged = Object.assign({}, JSON.parse(source));
+
   merged.start_url = options.production.start_url;
   merged.icons[0].src = options.production.icon_src;
 
   const mergedJSON = JSON.stringify(merged);
+
   this.emitFile('manifest.json', mergedJSON);
 
   return mergedJSON;
